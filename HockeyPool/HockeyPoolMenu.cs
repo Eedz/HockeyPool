244c7252ca2752baca4c929e605382e390d7f79a
eddiebauer0 <eddiebauer0@gmail.com>
2018-03-13 20:55:59 -0400

    Updated

--- HockeyPool/HockeyPoolMenu.cs
+++ HockeyPool/HockeyPoolMenu.cs
@@ -21,7 +21,7 @@ namespace HockeyPool
 
         private void HockeyPoolMenu_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'hockeyDataDataSet.People' table. You can move, or remove it, as needed.
+            
             this.peopleTableAdapter.Fill(this.hockeyDataDataSet.People);
 
             
